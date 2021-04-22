f78b16f3535ad644fd681f741a61b8efbe1348af
Sergii Maksymenko <sergiomaksymenko@gmail.com>
2021-04-22 20:11:43 +0200

    mvp

--- src/index.js
+++ src/index.js
@@ -1,1 +1,53 @@
 import './styles.css';
+import imagesList from './templates/imagesList.hbs';
+import ImagesApiService from './js/apiService';
+import LoadMoreBtn from './js/components/load-more-btn';
+
+const refs = {
+  searchQuery: document.querySelector('.form'),
+  gallery: document.querySelector('.gallery'),
+};
+
+const loadMoreBtn = new LoadMoreBtn({
+  selector: '[data-action="load-more"]',
+  hidden: true,
+});
+
+loadMoreBtn.enable();
+
+const imagesApiService = new ImagesApiService();
+
+refs.searchQuery.addEventListener('submit', onSearch);
+loadMoreBtn.refs.button.addEventListener('click', fetchGallery);
+
+function onSearch(e) {
+  e.preventDefault();
+
+  imagesApiService.query = e.target.elements.query.value;
+
+  //TODO Поправить уловие проврки!!!
+
+  if (imagesApiService.query.trim() === '') {
+    return alert('GG');
+  }
+  loadMoreBtn.show();
+  imagesApiService.resetPage();
+  clearImagesContainer();
+  fetchGallery();
+}
+
+function fetchGallery() {
+  loadMoreBtn.disable();
+  imagesApiService.fetchImages().then(hits => {
+    appendImagesMarkup(hits);
+    loadMoreBtn.enable();
+  });
+}
+
+function appendImagesMarkup(hits) {
+  refs.gallery.insertAdjacentHTML('beforeend', imagesList(hits));
+}
+
+function clearImagesContainer() {
+  refs.gallery.innerHTML = '';
+}
---
+++ src/js/apiService.js
@@ -0,0 +1,37 @@
+const API_KEY = '20675292-983eeb99019a7539d0696693a';
+const BASE_URL =
+  'https://pixabay.com/api/?image_type=photo&orientation=horizontal&q';
+
+export default class ImagesApiService {
+  constructor() {
+    this.searchQuery = '';
+    this.page = 1;
+  }
+
+  fetchImages() {
+    return fetch(
+      `${BASE_URL}=${this.searchQuery}&page=${this.page}&per_page=12&key=${API_KEY}`,
+    )
+      .then(response => response.json())
+      .then(({ hits }) => {
+        this.incrementPage();
+
+        return hits;
+      });
+  }
+
+  incrementPage() {
+    this.page += 1;
+  }
+
+  resetPage() {
+    this.page = 1;
+  }
+  get query() {
+    return this.searchQuery;
+  }
+
+  set query(newQuery) {
+    this.searchQuery = newQuery;
+  }
+}
